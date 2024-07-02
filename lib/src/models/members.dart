5238c1bce077ed0c3c0096601c70cd5337935d5a
Abrar Mahir Esam <87098848+Abrar118@users.noreply.github.com>
2024-07-02 07:12:48 +0600

    nishur part tar error thik kora shesh

---
+++ lib/src/models/members.dart
@@ -0,0 +1,20 @@
+class Members {
+  final String image;
+  final String name;
+  final String points;
+
+  Members(
+    this.image,
+    this.name,
+    this.points,
+  );
+}
+
+List members = [
+  Members('lib/assets/img/avatar4.png', 'Tamara Schmidt', '36 pts'),
+  Members('lib/assets/img/avatar5.png', 'Richard Schod', '36 pts'),
+  Members('lib/assets/img/avatarme.png', 'You ', '36 pts'),
+  Members('lib/assets/img/avatar6.png', 'Gary SChimo', '38 pts'),
+  Members('lib/assets/img/avatar7.png', 'Becky Nick', '40 pts'),
+  Members('lib/assets/img/avatar8.png', 'Jhon Cleark', '42 pts'),
+];
