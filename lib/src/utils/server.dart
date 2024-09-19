c80c629c00a91ef23ce994b48b3d265cf5454a93
Shadman Shafeen <shadmanskystar@gmail.com>
2024-09-19 22:14:14 +0600

    fixing branches

--- lib/src/utils/server.dart
+++ lib/src/utils/server.dart
@@ -1,5 +1,5 @@
 const serverURLFlask = 'http://192.168.10.162:5000';
-const serverURLExpress = 'http://10.103.133.215:3000';
+const serverURLExpress = 'http://192.168.10.162:3000';
 
 //10.103.133.215
 //192.168.10.162
